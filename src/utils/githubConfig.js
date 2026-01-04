c72f870ebfbed7bfe3724b22609be877d633d7f3
alansong49 <alansong49@gmail.com>
2026-01-04 15:42:13 +0800

    Update githubConfig.js

--- src/utils/githubConfig.js
+++ src/utils/githubConfig.js
@@ -4,8 +4,6 @@ export const GITHUB_CONFIG = {
   REPO: import.meta.env.VITE_GITHUB_REPO || 'leleo-Silas',
   BRANCH: import.meta.env.VITE_GITHUB_BRANCH || 'main',
   APP_ID: import.meta.env.VITE_GITHUB_APP_ID || '2590622',
-  ENCRYPT_KEY: import.meta.env.VITE_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
-  TOKEN: import.meta.env.VITE_GITHUB_TOKEN || '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',
   FILE_PATH: 'src/config.js' // 要更新的配置文件路径
 };
 
