4195b6331439b8709c106107159240ff7ac3a1a4
alansong49 <alansong49@gmail.com>
2026-01-04 15:08:45 +0800

    Update githubConfig.js

--- src/utils/githubConfig.js
+++ src/utils/githubConfig.js
@@ -3,7 +3,7 @@ export const GITHUB_CONFIG = {
   OWNER: import.meta.env.VITE_GITHUB_OWNER || 'alansong49',
   REPO: import.meta.env.VITE_GITHUB_REPO || 'leleo-Silas',
   BRANCH: import.meta.env.VITE_GITHUB_BRANCH || 'main',
-  APP_ID: import.meta.env.VITE_GITHUB_APP_ID || '2569770',
+  APP_ID: import.meta.env.VITE_GITHUB_APP_ID || '2590622',
   ENCRYPT_KEY: import.meta.env.VITE_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
   TOKEN: import.meta.env.VITE_GITHUB_TOKEN || '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',
   FILE_PATH: 'src/config.js' // 要更新的配置文件路径
